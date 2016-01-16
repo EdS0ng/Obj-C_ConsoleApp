6a66d3bec1755aa5a79d8b75abf57ed799407370
Edward Song <edwardsong@edwards-mac.local>
2016-01-16 12:28:52 -0800

    Initial Commit

---
+++ CourseManagementConsoleApp/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  CourseManagementConsoleApp
+//
+//  Created by Edward Song on 1/16/16.
+//  Copyright © 2016 Edward Song. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
